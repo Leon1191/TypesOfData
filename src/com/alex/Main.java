8046bcb3054d3162b610bc1792531db4354e4b1b
Leo <stbirb@yandex.ru>
2019-05-09 16:13:00 +0300

    Initial commit

---
+++ src/com/alex/Main.java
@@ -0,0 +1,31 @@
+package com.alex;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+         int myIntNumber = 500;
+
+         byte myByteNumber = 100;
+
+         short myShortNumber = 25000;
+
+         long myLongNumver = (long) 50000 + 10 * (myByteNumber + myShortNumber + myIntNumber);
+
+         System.out.println("myLongNumver = "+myLongNumver);
+
+
+
+
+
+
+
+
+
+
+
+
+	// write your code here
+    }
+}
